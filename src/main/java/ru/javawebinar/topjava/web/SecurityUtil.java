1644b96e078847707dbc384e7839282da773bdad
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2023-02-24 18:57:54 +0200

    4_5_improve_code

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -3,8 +3,10 @@ package ru.javawebinar.topjava.web;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
-
     private static int id = 1;
+
+    private SecurityUtil() {
+    }
 
     public static int authUserId() {
         return id;
