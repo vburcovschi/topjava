3a65775b646d4136f044243e0bd313ff814dc2cd
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2023-04-16 17:41:36 +0300

    9_12_spring_security_test

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -15,12 +15,16 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static ru.javawebinar.topjava.TestUtil.mockAuthorize;
+import static ru.javawebinar.topjava.UserTestData.user;
+
 public class SpringMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/inmemory.xml");
         System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
         AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
         adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+        mockAuthorize(user);
         System.out.println();
         MealRestController mealController = appCtx.getBean(MealRestController.class);
         List<MealTo> filteredMealsWithExcess =
